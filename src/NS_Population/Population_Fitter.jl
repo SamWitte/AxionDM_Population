e8fc3a0d53aa1bce4bc3b939cd0255187e66f7c7
Samuel Witte <witte.sam@gmail.com>
2023-10-22 12:18:11 +0100

    update speed

--- src/NS_Population/Population_Fitter.jl
+++ src/NS_Population/Population_Fitter.jl
@@ -9,6 +9,10 @@ using Distributions
 using LSODA
 using StatsBase
 using Suppressor
+using Dates
+using Distributed
+using SharedArrays
+
 # using Flux
 # using SciMLSensitivity
 
@@ -19,9 +23,17 @@ pygedm = pyimport("pygedm")
 
 
 Random.seed!(1235)
+run_analysis = false
+fileName = "Test_Run"
+
+run_plot_data = true
+if run_plot_data
+    fileName = "Sample_population"
+end
+xIn = [0.2, log10.(8e12), 0.15, 0.4, 0.0]
 
 run_magnetars = false
-fileName = "Test_Run"
+
 
 Pmin=0.05
 Pmax=0.9
@@ -42,9 +54,7 @@ NPts_Bsig=5
 tau_ohmic = 10.0e6  # yrs
 max_T = 5.0 * tau_ohmic
 
-Nsamples = 200000 # samples per point
-
-
+Nsamples = 2000000 # samples per point
 
 filePulsars = open(readdlm,"psrcat_tar/store_out/Pulsar_Population.txt")
 fileMagnetars = open(readdlm, "psrcat_tar/store_out/Magnetar_Population.txt")
@@ -67,6 +77,8 @@ true_pop = true_pop[true_pop[:,2] .> 0, :]
 B_true = 1e12 .* sqrt.((true_pop[:, 2] ./ 1e-15) .* true_pop[:, 1])
 true_pop = true_pop[(B_true .> B_min) .& (B_true .< B_max), :]
 
+
+
 rval = cor(true_pop[:,1], true_pop[:,2])
 # print("Correlation Coeff \t ", rval, "\n")
 
@@ -87,8 +99,9 @@ function beaming_cut(P)
 end
 
 function Lum(P, Pdot, dist; alpha=0.48, muLcorr=0.0, sigLcorr=0.8)
-    L0 = 5.69e6
-    L1Ghz = L0 * 10^randn() * (Pdot / P^3)^alpha
+    L0 = 5.69e6 # mJy / kpc^2
+    d = Normal(0.0, 0.8)
+    L1Ghz = L0 * 10 .^rand(d,1)[1] * (Pdot / P^3)^alpha
     return L1Ghz, L1Ghz / dist^2
 end
 
@@ -113,9 +126,11 @@ function min_flux_test1(P, tau_sc; threshold=0.1)
     end
 end
 
-function min_flux_test2(P, DM; beta=2, tau=1.0e-2, DM0=60, DM1=7.5, G=0.64, Trec=21, d_freq=10e6, tobs=200, SN=10, Tsky=1.0)
+function min_flux_test2(P, DM; beta=2, tau=1.0e-2, DM0=60, DM1=7.5, G=0.64, Trec=21, d_freq=100e6, tobs=200, SN=10, Tsky=1.0)
+    #CHECK!
     wid = 0.05 * P
-    Smin = SN * beta * (Trec + Tsky) / (G * 2 * d_freq * tobs) * 1e3
+    Smin = SN * beta * (Trec + Tsky) / (G * sqrt.(2 * d_freq * tobs)) .* 1e3
+    
     w_e = sqrt.(wid^2 + (beta * tau)^2 + (tau * DM / DM0)^2 + (tau * (DM - DM1) / DM1)^2)
     if w_e .>= P
         return 1e100
@@ -131,19 +146,24 @@ function evolve_pulsar(B0, P0, Theta_in, age; n_times=1e2, beta=6e-40, tau_Ohm=1
     tspan = (1, age)
     saveat = (tspan[2] .- tspan[1]) ./ n_times
     
-    prob = ODEProblem(RHS!, y0, tspan, Mvars, reltol=1e-4, abstol=1e-4)
-    # sol = @suppress solve(prob, lsoda(), saveat=saveat)
-    # sol = solve(prob, Vern6(), saveat=saveat)
-    sol = solve(prob, Tsit5(), saveat=saveat)
-    # sol = solve(prob, Euler(), saveat=saveat, dt=5000.0)
-    
-    
     Bf = B0 .* exp.(- age ./ tau_Ohm);
+    
+    prob = ODEProblem(RHS!, y0, tspan, Mvars, reltol=1e-4, abstol=1e-4)
+    
+    condition_r(u,lnt,integrator) = u[2]
+    function affect_r!(integrator)
+        integrator.u[2] = 0.0
+    end
+    cb_r = ContinuousCallback(condition_r, affect_r!)
+    sol = solve(prob, Vern6(), saveat=saveat)
+    
+    
     Pf = sol.u[end][1]
     if Pf .< P0
         Pf = P0
     end
     Theta_f = sol.u[end][2]
+    # print("Sol \t ", Pf, "\t", Theta_f, "\n")
     
     Pdot = beta .* Bf.^2 ./ Pf .* (1.0 .+ 1.0 .* sin.(Theta_f).^2)
     return Bf, Pf, Theta_f, Pdot
@@ -151,6 +171,7 @@ end
 
 
 function RHS!(du, u, Mvars, t)
+    
     beta, tau_Ohm, B0 = Mvars
     
     B = B0 .* exp.(- t ./ tau_Ohm);
@@ -161,13 +182,14 @@ function RHS!(du, u, Mvars, t)
     P = u[1]
     chi = mod(u[2], pi/2)
     
+    
     alive = (B / P^2 > 0.34 * 1e12)
     
     # du[1] = -1.0 / tau_Ohm
     
     du[1] = beta * B.^2 ./ P * (alive * 1.0 + 1.0 * sin(chi)^2) .* 3.1536e+7
     du[2] = -1.0 * beta * B^2 / P^2 * sin(chi) * cos(chi) .* 3.1536e+7
-    
+
     return
 end
 
@@ -194,10 +216,13 @@ end
 
 function simulate_pop(num_pulsars, ages; beta=6e-40, tau_Ohm=10.0e6, width_threshold=0.1, pulsar_data=nothing)
     
-    final_list = []
+    # final_list = []
     Theta_in = draw_chi(length(ages))
     
-    for i in 1:length(ages)
+    temp_store = SharedArray{Float64}(length(ages), 7)
+    
+    Threads.@threads for i in 1:length(ages)
+    # @distributed for i in 1:length(ages)
         
         if typeof(pulsar_data) == Nothing
             B0 = draw_Bfield_lognorm()
@@ -208,61 +233,65 @@ function simulate_pop(num_pulsars, ages; beta=6e-40, tau_Ohm=10.0e6, width_thres
         end
         
         xfin = sample_location(ages[i], diskH=0.5, diskR=10.0)
-#        if abs(xfin[3]) > 0.5
-#            # print("z \t", xfin, " \n")
-#            continue
-#        end
         xE = [0.0, 8.3, 0.0]
         xfin_shift = xfin .- xE
         dist_earth = sqrt.(sum(xfin_shift.^2))
         
-        # practical cuts
-        if (dist_earth .> 6)&&(B0 .* exp.(ages[i] ./ tau_Ohm) .< 4.4e13)
+        Bf = B0 .* exp.(- ages[i] ./ tau_Ohm)
+        Bdeath = 0.34 * 1e12 * P0.^2
+        if Bf <= Bdeath
             continue
         end
-    
         
         Bf, Pf, ThetaF, Pdot = evolve_pulsar(B0, P0, Theta_in[i], ages[i], beta=beta, tau_Ohm=tau_Ohm)
-        
         Bdeath = 0.34 * 1e12 * Pf.^2
         if Bf <= Bdeath
-            # print("death? \n")
             continue
         end
-
+        
         GC_b = asin.(xfin[3] / dist_earth)
         GC_l = atan.(xfin_shift[1], xfin_shift[2])
-        DM, tau_sc = pygedm.dist_to_dm(GC_l, GC_b, dist_earth * 1e3 * u.pc, method="ymw16")
-        if Bf < 4.4e13
-            
-            if beaming_cut(Pf) == 0
-                continue
-            end
-            lum, s_den1GHz = Lum(Pf, Pdot, dist_earth, alpha=0.48, muLcorr=0.0, sigLcorr=0.8)
-            
-            minF1 = min_flux_test1(Pf, tau_sc[1], threshold=width_threshold)
-            if minF1 == 0
-                continue
-            end
-            minF2 = min_flux_test2(Pf, DM[1], beta=2, tau=1.0e-2, DM0=60, DM1=7.5, G=0.64, Trec=21, d_freq=10e6, tobs=200, SN=10, Tsky=1.0)
-            if s_den1GHz < minF2
-                continue
-            end
-        else
-            s_den1GHz = 1.0
-        end
         
-        push!(final_list, [Pf, Pdot])
+        temp_store[i, :] = [ages[i], Bf, Pf, Pdot, GC_b, GC_l, dist_earth]
     end
     
-    return reduce(hcat, final_list)'
+    temp_store = temp_store[temp_store[:,1] .> 0.0, :]
+    
+    final_list = SharedArray{Float64}(length(temp_store[:,1]), 2)
+    for i in 1:length(temp_store[:,1])
+        age, Bf, Pf, Pdot, GC_b, GC_l, dist_earth = temp_store[i, :]
+
+        if beaming_cut(Pf) == 0
+            continue
+        end
+        lum, s_den1GHz = Lum(Pf, Pdot, dist_earth, alpha=0.48, muLcorr=0.0, sigLcorr=0.8)
+        
+        DM, tau_sc = pygedm.dist_to_dm(GC_l, GC_b, dist_earth * 1e3 * u.pc, method="ymw16")
+        
+        minF1 = min_flux_test1(Pf, tau_sc[1], threshold=width_threshold)
+        if minF1 == 0
+            continue
+        end
+        minF2 = min_flux_test2(Pf, DM[1], beta=2, tau=1.0e-2, DM0=60, DM1=7.5, G=0.64, Trec=21, d_freq=10e6, tobs=200, SN=10, Tsky=1.0)
+        
+        if s_den1GHz < minF2
+            continue
+        end
+        
+        
+        # push!(final_list, [Pf, Pdot])
+        final_list[i, :] = [Pf, Pdot]
+    end
+    
+    final_out = final_list[final_list[:,1] .> 0.0, :]
+    return final_out
 end
 
 
-function likelihood_func(theta, real_samples, rval; npts_cdf=100)
+function likelihood_func(theta, real_samples, rval; npts_cdf=50)
     mu_P, mu_B, sig_P, sig_B, cov_PB = theta
     mean = [mu_P, mu_B]
-    cov = [sig_P cov_PB; cov_PB sig_B]
+    cov = [sig_P.^2 cov_PB.^2; cov_PB.^2 sig_B.^2]
     dist = MvNormal(mean, cov)
     out_samps = rand(dist, Nsamples)'
     
@@ -402,7 +431,34 @@ function minimization_scan(real_samples, rval; max_T=1e7)
     return qval_L
 end
 
-# minimization_scan(true_pop, rval; max_T=max_T)
-outputTable = hard_scan(true_pop, rval, max_T=max_T, Pmin=Pmin, Pmax=Pmax, Bmin=Bmin, Bmax=Bmax, sigP_min=sigP_min, sigP_max=sigP_max, sigB_min=sigB_min, sigB_max=sigB_max, Npts_P=Npts_P, Npts_B=Npts_B, NPts_Psig=NPts_Psig, NPts_Bsig=NPts_Bsig)
 
-writedlm(fileName*".dat", outputTable)
+
+time0=Dates.now()
+
+if run_analysis
+    # minimization_scan(true_pop, rval; max_T=max_T)
+    outputTable = hard_scan(true_pop, rval, max_T=max_T, Pmin=Pmin, Pmax=Pmax, Bmin=Bmin, Bmax=Bmax, sigP_min=sigP_min, sigP_max=sigP_max, sigB_min=sigB_min, sigB_max=sigB_max, Npts_P=Npts_P, Npts_B=Npts_B, NPts_Psig=NPts_Psig, NPts_Bsig=NPts_Bsig)
+
+    writedlm(fileName*".dat", outputTable)
+end
+
+if run_plot_data
+    mu_P, mu_B, sig_P, sig_B, cov_PB = xIn
+    mean = [mu_P, mu_B]
+    cov = [sig_P cov_PB; cov_PB sig_B]
+    dist = MvNormal(mean, cov)
+    out_samps = rand(dist, Nsamples)'
+    
+    
+    P_in = abs.(out_samps[:,1]) # 10 .^(out_samps[:,1])
+    B_in = 10 .^(out_samps[:,2])
+    data_in = hcat(B_in, P_in)
+    ages = rand(0:max_T, length(B_in))
+    
+    out_pop = simulate_pop(Nsamples, ages, tau_Ohm=tau_ohmic, pulsar_data=data_in)
+    writedlm(fileName*".dat", out_pop)
+    writedlm(fileName*"_IN.dat", out_samps)
+end
+
+time1=Dates.now()
+print("\n\n Run time: ", time1-time0, "\n")
